030a80b2fd67b386b4da8621b05482fdb7907eba
NavardoW <navardowilliamsrei@gmail.com>
2026-01-29 13:07:47 -0500

    test

--- config/nvim/lua/vardo/config/lazy.lua
+++ config/nvim/lua/vardo/config/lazy.lua
@@ -15,6 +15,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	spec = "vardo.plugins",
+	spec = {
+        { import = "vardo.plugins" },
+        { import = "vardo.plugins.lsp" }
+    }
 })
 
